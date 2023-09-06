2e473367b28a299a1cc746628d44d443661a50a1
André <andre.marcelino@parnamirim.rn.gov.br>
2023-09-06 14:34:29 -0300

    + YesNoButton integrado com Reactive Forms

--- src/app/shared/componentes/yes-no-button-group/yes-no-button-group.component.ts
+++ src/app/shared/componentes/yes-no-button-group/yes-no-button-group.component.ts
@@ -1,22 +1,47 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output, forwardRef } from '@angular/core';
+import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
 
 @Component({
   selector: 'app-yes-no-button-group',
   templateUrl: './yes-no-button-group.component.html',
   styleUrls: ['./yes-no-button-group.component.scss'],
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      multi: true,
+      useExisting: forwardRef(() => YesNoButtonGroupComponent),
+    },
+  ],
 })
-export class YesNoButtonGroupComponent implements OnInit {
+export class YesNoButtonGroupComponent implements OnInit, ControlValueAccessor {
   @Input() value: string = null;
   @Input() label: string = null;
   @Output() valueChange = new EventEmitter<string>();
-  public options = YesNoButtonGroupOptions;
+  options = YesNoButtonGroupOptions;
+  onChange = (value: string) => {};
+  onTouched = () => {};
 
   constructor() {}
 
   ngOnInit(): void {}
 
+  writeValue(value: string): void {
+    this.value = value;
+    this.onChange(this.value);
+  }
+  registerOnChange(fn: any): void {
+    this.onChange = fn;
+  }
+  registerOnTouched(fn: () => void): void {
+    this.onTouched = fn;
+  }
+  setDisabledState?(isDisabled: boolean): void {
+    throw new Error('Method not implemented.');
+  }
+
   public activate(value: string) {
     this.value = value;
+    this.writeValue(this.value);
     this.valueChange.emit(this.value);
   }
 }
